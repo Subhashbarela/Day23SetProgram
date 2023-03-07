e537cd4e7db23541928eb659afce4610fe5a8922
Subhash Barela <barelasubhash16@gmail.com>
2023-03-07 20:03:10 +0530

    welcome to generic collection program

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day22CollectionProject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("welcome to Day 23 set Program ");
+        }
+    }
+}
