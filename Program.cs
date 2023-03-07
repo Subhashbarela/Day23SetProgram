d5a352ecb91a03b340333d508c6118576dae0837
Subhash Barela <barelasubhash16@gmail.com>
2023-03-07 20:19:49 +0530

    contact form using collection

--- Program.cs
+++ Program.cs
@@ -1,16 +1,26 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics.Contracts;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Day22CollectionProject
+namespace Day23CollectionProject
 {
-    internal class Program
+    public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("welcome to Day 23 set Program ");
+            AddressBook myAddressBook = new AddressBook();
+            Contacts newContact = new Contacts("Prince", "Praveen", "INDIA", "8369319177", "prince.p@example.com");
+            Contacts newContact2 = new Contacts("Robert", "Downey", "USA", "888222444", "robery.downey@jr.com");
+            Contacts newContact3 = new Contacts("Robert", "Downey", "USA", "888222444", "robery.downey@jr.com");
+
+            myAddressBook.AddContact(newContact);
+            myAddressBook.AddContact(newContact2);
+            myAddressBook.AddContact(newContact3);
+            myAddressBook.PrintContacts();
         }
     }
+    
 }
